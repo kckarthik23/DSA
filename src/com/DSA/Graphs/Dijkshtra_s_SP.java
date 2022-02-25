fe9080e54a2dd927ad26545a6f0047ff324b3c25
kckarthik23 <karthikraokc@gmail.com>
2022-02-26 01:58:04 +0530

    Formatted the DSP code

--- src/com/DSA/Graphs/Dijkshtra_s_SP.java
+++ src/com/DSA/Graphs/Dijkshtra_s_SP.java
@@ -9,11 +9,11 @@ public class Dijkshtra_s_SP {
     int source;
 
     Dijkshtra_s_SP(int length, int source) {
-        this.length=length;
+        this.length = length;
         this.spSet = new boolean[length];
         this.dist = new int[length];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        this.source=source;
+        this.source = source;
         this.dist[source] = 0;
     }
 
@@ -30,24 +30,20 @@ public class Dijkshtra_s_SP {
         return minIndex;
     }
 
-    public void dijKshtraAlgo(int dist[],int graph[][])
-    {
-        for(int c=0;c<length-1;c++)
-        {
-           int minInd=minEdge(dist);
-           spSet[minInd]=true;
-           
-           for(int j=0;j<length;j++)
-           {
-               if(!spSet[j]&&graph[minInd][j]!=0)
-               dist[j]=Math.min(dist[j], dist[minInd]+graph[minInd][j]);
-           }
+    public void dijKshtraAlgo(int dist[], int graph[][]) {
+        for (int c = 0; c < length - 1; c++) {
+            int minInd = minEdge(dist);
+            spSet[minInd] = true;
+
+            for (int j = 0; j < length; j++) {
+                if (!spSet[j] && graph[minInd][j] != 0)
+                    dist[j] = Math.min(dist[j], dist[minInd] + graph[minInd][j]);
+            }
         }
         print(dist);
     }
 
-    public void print(int dist[])
-    {
+    public void print(int dist[]) {
         for (int i : dist) {
             System.out.println(i);
         }
@@ -55,12 +51,12 @@ public class Dijkshtra_s_SP {
 
     public static void main(String[] args) {
         int graph[][] = new int[][] { { 0, 2, 0, 6, 0 },
-                                      { 2, 0, 3, 8, 5 },
-                                      { 0, 3, 0, 0, 7 },
-                                      { 6, 8, 0, 0, 9 },
-                                      { 0, 5, 7, 9, 0 } };
+                { 2, 0, 3, 8, 5 },
+                { 0, 3, 0, 0, 7 },
+                { 6, 8, 0, 0, 9 },
+                { 0, 5, 7, 9, 0 } };
 
-          Dijkshtra_s_SP dsp=new Dijkshtra_s_SP(5, 1);
-          dsp.dijKshtraAlgo(dsp.dist, graph);                            
+        Dijkshtra_s_SP dsp = new Dijkshtra_s_SP(5, 1);
+        dsp.dijKshtraAlgo(dsp.dist, graph);
     }
 }
