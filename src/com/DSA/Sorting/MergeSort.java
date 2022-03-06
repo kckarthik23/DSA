737328f39f9749635293b1894a4b3183faa298e5
kckarthik23 <karthikraokc@gmail.com>
2022-03-07 00:40:02 +0530

    MergeSort and merge sort using multithrading, multithrading merge sort is still pending

--- src/com/DSA/Sorting/MergeSort.java
+++ src/com/DSA/Sorting/MergeSort.java
@@ -2,28 +2,74 @@ package com.DSA.Sorting;
 
 public class MergeSort {
 
-    public static int[] mergeSort(int arr[],int start,int last) {
+    public static int[] mergeSort(int arr[], int start, int last) {
 
-        while(start<last)
-        {
-            int mid=start+(last-start)/2;
-            mergeSort(arr,start,mid);
-            mergeSort(arr,mid+1, last);
-            merge(arr,start,mid,last);
+        if (start < last) {
+            int mid = start + (last - start) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, last);
+            merge(arr, start, mid, last);
         }
-        return null;
+        return arr;
     }
 
-    private static void merge(int[] arr, int start, int mid,int last) {
-        int arr1[]=new int[mid-start+1];
-        int arr2[]=new int[last-mid];
-        
-        for(int i=0;i<mid;i++)
-        arr1[i]=arr[start+i];
+    // Used more variables so needs memory efficient solution
+    private static void merge(int[] arr, int start, int mid, int last) {
+        int arr1[] = new int[mid - start + 1];
+        int arr2[] = new int[last - mid];
+        int temp[] = new int[last - start + 1];
+        for (int i = 0; i <= mid - start; i++)
+            arr1[i] = arr[start + i];
 
-        for(int j=0;j<mid;j++)
-        arr1[j]=arr[last-mid];
+        for (int j = 0; j < last - mid; j++)
+            arr2[j] = arr[mid + j + 1];
+        int m = arr1.length;
+        int n = arr2.length;
+        int k = 0;
+        int h = 0;
+        int l = 0;
+        while (h < m && l < n) {
+            if (arr1[h] > arr2[l]) {
+                temp[k] = arr2[l];
+                l++;
+
+            } else {
+
+                temp[k] = arr1[h];
+                h++;
+            }
+
+            k++;
+        }
+
+        while (h < m) {
+
+            temp[k] = arr1[h];
+            h++;
+            k++;
+        }
+
+        while (l < n) {
+
+            temp[k] = arr2[l];
+            l++;
+            k++;
+        }
+        for (int a = 0; a < temp.length; a++)
+            arr[a + start] = temp[a];
+
+        /*
+         * for (int val : arr)
+         * System.out.println(val);
+         */
 
     }
-    
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 7, 2, 9, 0, 3, 1, 8, 6, 4 };
+        int res[] = mergeSort(arr, 0, arr.length - 1);
+        for (int finalVal : res)
+            System.out.println(finalVal);
+
+    }
 }
--- src/com/java/multithreading/MultiThreadingDemo.java
+++ src/com/java/multithreading/MultiThreadingDemo.java
@@ -2,51 +2,83 @@ package com.java.multithreading;
 
 public class MultiThreadingDemo {
 
-    public static void parallelExecute(int low, int high, int numThreads) {
+    public static void parallelExecute(int arr[], int low, int high, int numThreads) {
+        int mid = low + (high - low) / 2;
+
         if (numThreads <= 1) {
-            printNum(low, high);
+            merge(arr, mid, low, high);
             return;
         }
-        int mid = (low + high )/ 2;
-        Thread thLower = createThread(low, mid, numThreads);
-        Thread thHigher = createThread(mid, high, numThreads);
+        Thread thLower = createThread(arr, low, mid, numThreads);
+        Thread thHigher = createThread(arr, mid + 1, high, numThreads);
         thLower.setName("thLower");
         thHigher.setName("thHigher");
         thLower.start();
         thHigher.start();
-        /* try {
-            thLower.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            
-            
+        /*
+         * try {
+         * thLower.join();
+         * } catch (InterruptedException e) {
+         * // TODO Auto-generated catch block
+         * e.printStackTrace();
+         * 
+         * 
+         * }
+         * try {
+         * thHigher.join();
+         * } catch (InterruptedException e) {
+         * // TODO Auto-generated catch block
+         * e.printStackTrace();
+         * }
+         */
+       // System.out.println(" main Thread is finished");
+    }
+
+    private static void merge(int arr[], int mid, int low, int high) {
+        int temp[] = new int[high - low + 1];
+        for (int i = 0; i <= high - low; i++)
+            temp[i] = arr[low + i];
+
+        int i = 0;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= high) {
+            if (temp[i] > temp[j]) {
+                arr[i + low] = temp[j];
+                j++;
+            } else {
+                arr[i + low] = temp[i];
+                i++;
+            }
+            k++;
         }
-        try {
-            thHigher.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }*/
-        System.out.println(" main Thread is finished");
+
+        while (i <= mid) {
+            arr[i + low] = temp[i];
+        }
+
+        while (j <= high) {
+            arr[j] = temp[j];
+        }
+
     }
 
-    private static void printNum(int low, int high) {
-        System.out.println("low is " + low + " high is " + high);
-    }
-
-    private static Thread createThread(int low, int high, int numThreads) {
+    private static Thread createThread(int arr[], int low, int high, int numThreads) {
         return new Thread() {
             public void run() {
-              System.out.println(this.getName());
-                parallelExecute(low, high, numThreads / 2);
+                System.out.println(this.getName());
+                parallelExecute(arr, low, high, numThreads / 2);
 
             }
         };
     }
-public static void main(String[] args) {
-    int numThreads=4;//Runtime.getRuntime().availableProcessors();
-   // System.out.println(numThreads);
-    parallelExecute(0, 16, numThreads);
-}
+
+    public static void main(String[] args) {
+        int numThreads = 4;// Runtime.getRuntime().availableProcessors();
+        // System.out.println(numThreads);
+        int arr[] = { 9, 5, 0, 6, 1, 7, 2, 4, 8, 3 };
+        parallelExecute(arr, 0, arr.length - 1, numThreads);
+        for(int val:arr)
+        System.out.println(val);
+    }
 }
